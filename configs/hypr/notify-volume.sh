3078f5f60309c4574d29eca1694928c016d51d8f
Manh Nguyen <manhnb1911@gmail.com>
2025-09-22 12:12:09 +0700

    .

--- configs/hypr/notify-volume.sh
+++ configs/hypr/notify-volume.sh
@@ -21,6 +21,6 @@ else
 fi
 
 name=$(wpctl inspect @DEFAULT_SINK@ | grep node.description | awk -F'"' '{print $2}')
-[ ${#name} -gt 16 ] && name="${name:0:13}..." 
+[ ${#name} -gt 18 ] && name="${name:0:15}..." 
 
 notify-send -a "notify-volume" -r 50 "Volume: $status" "$name" -h int:value:$volume -i $icon
