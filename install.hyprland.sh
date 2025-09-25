6cc023ad281b5b61282dd962ff995ae4da4b862d
Manh Nguyen <manhnb1911@gmail.com>
2025-09-25 15:02:10 +0700

    .

--- install.hyprland.sh
+++ install.hyprland.sh
@@ -7,6 +7,12 @@ echo "Installing Hyprland packages..."
 install - < packages/hyprland.txt
 
 
+echo "Installing Nautilus..."
+yay -Rcns --noconfirm dolphin
+install nautilus nautilus-open-any-terminal
+dconf write /com/github/stunkymonkey/nautilus-open-any-terminal/terminal "'kitty'"
+
+
 echo "Creating symlinks for dotfiles..."
 link hypr ~/.config/hypr
 link waybar ~/.config/waybar
